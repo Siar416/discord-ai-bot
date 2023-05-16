9a34cdd03b7580099c2e7f9031610d1b2190a7ac
Siar Wahidi <siar.wahidi@gmail.com>
2023-05-16 16:49:54 -0400

    Setup openai config to communicate with openai api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv/config");
 const { Client, IntentsBitField } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
 
 const client = new Client({
   intents: [
@@ -14,8 +15,32 @@ client.on("ready", () => {
   console.log("Ai bot is online!");
 });
 
+// Configuration need to communicate with openai
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Event to listen for messages
 client.on("messageCreate", (message) => {
-  console.log(message);
+  // will not respond to bots
+  if (message.author.bot) return;
+
+  // bot will only respond to messages to specified server/channel
+  if (message.channel.id !== process.env.CHANNEL_ID) return;
+
+  // bot will only get triggered if message starts with !ai
+  if (!message.content.startsWith("!ai")) return;
+
+  let conversationLog = [
+    { role: "system", content: "You are a friendly chatbot." },
+  ];
+
+  conversationLog.push({
+    role: "user",
+    content: message.content,
+  });
 });
 
 client.login(process.env.TOKEN);
